5d16f9520e7fd7da7422865e6cf52a36fd744794
Justakazh <36891477+justakazh@users.noreply.github.com>
2022-06-02 04:38:54 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+
+
+apt update
+apt install git
+wget https://go.dev/dl/go1.18.3.linux-amd64.tar.gz 
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.18.3.linux-amd64.tar.gz
+cp /usr/local/go/bin/go /usr/local/bin
+go --version
+go install github.com/ffuf/ffuf@latest
+go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
+go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+mv ~/go/bin/nuclei /usr/local/bin
+mv ~/go/bin/httpx /usr/local/bin
+mv ~/go/bin/ffuf /usr/local/bin
+git clone https://github.com/justakazh/yusub
+chmod +x /yusub/yusub
+mv /yusub/yusub /usr/local/bin
+git clone https://github.com/justakazh/yuyu_scanner
+chmod +x /yuyu_scanner/yuyu
+mv /yuyu_scanner/yuyu /usr/local/bin
